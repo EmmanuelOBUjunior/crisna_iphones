72b74f7bec340d7d9936e9633420026720404357
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2020-07-02 16:06:10 +0100

    done with my cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,6 +4,7 @@ import CartColumns from './CartColumns'
 import EmptyCart from './EmptyCart'
 import {ProductConsumer} from '../../context'
 import CartList from './CartList'
+import CartTotals from './CartTotals'
 
 
 class Cart extends Component {
@@ -20,6 +21,7 @@ class Cart extends Component {
                             <Title name ="your" title="cart" />
                             <CartColumns />
                             <CartList value={value}/>
+                            <CartTotals value= {value}></CartTotals>
                             </React.Fragment> 
                             );
                        }
